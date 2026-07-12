6628f0cfbdb16dd877b6dc60ba33cb8c76bccc2f
kubashh <jakubhanula0@gmail.com>
2026-07-12 22:48:07 +0200

    minify html by script

--- build.js
+++ build.js
@@ -1,7 +1,34 @@
-await Bun.build({
+const outpath = `./dist/index.html`;
+
+const { outputs } = await Bun.build({
   entrypoints: [`./src/index.html`],
   outdir: `.`,
-  naming: `./dist/index.html`,
+  naming: outpath,
   compile: true,
   minify: true,
 });
+
+// Bun makes empty script tag, so it can be removed to reduce the size of the output file
+const htmlWitoutScripts = removeScripts(await outputs[0].text());
+// Bun minify do not minify the html, so we need to minify it manually
+const minifiedHtml = minifyHtml(htmlWitoutScripts);
+await Bun.write(outputs[0].path, minifiedHtml);
+
+function removeScripts(text) {
+  return text.replaceAll(/<script[\s\S]*?<\/script>/g, ``);
+}
+
+function minifyHtml(text) {
+  return text
+    .replaceAll(/\/\*[\s\S]*?\*\//g, ``) // Remove comments
+    .replaceAll(`\n`, ` `)
+    .replaceAll(/\s{2,}/g, ` `)
+    .replaceAll(/ > | >|> /g, `>`)
+    .replaceAll(/ < | <|< /g, `<`)
+    .replaceAll(/ ; | ;|; /g, `;`)
+    .replaceAll(/ { | {|{ /g, `{`)
+    .replaceAll(/ } | }|} /g, `}`)
+    .replaceAll(/ " | "|" /g, `"`)
+    .replaceAll(/ , | ,|, /g, `,`)
+    .replaceAll(`: `, `:`); // color: red; => color:red;
+}
